43b9b1785a646000ea583c594fc9c1ec4d16ecec
ZenBarrier <anthony.j.barrera@gmail.com>
2016-06-29 00:00:05 -0400

    Splash Scene

---
+++ Assets/Scripts/SplashController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SplashController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Invoke("EndSplash", 5);
+	}
+
+    void EndSplash()
+    {
+        LevelManager.LoadNextLevel();
+    }
+}
