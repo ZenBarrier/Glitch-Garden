8d0a4e9574d3aa51fba88fe75509d4aaf5cc4db4
ZenBarrier <anthony.j.barrera@gmail.com>
2016-07-01 15:52:08 -0400

    Options Manager

---
+++ Assets/Scripts/OptionsManager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class OptionsManager : MonoBehaviour {
+
+    public Slider volumeSlider;
+    public Slider difficultySlider;
+    public LevelManager levelManager;
+
+    private MusicManager musicManager;
+
+    // Use this for initialization
+    void Start () {
+        musicManager = GameObject.FindObjectOfType<MusicManager>();
+        Debug.Log(musicManager);
+
+        volumeSlider.value = PlayerPrefsManager.GetMasterVolume();
+        difficultySlider.value = PlayerPrefsManager.GetDifficulty();
+	}
+
+    void Update()
+    {
+        musicManager.SetVolume(volumeSlider.value);
+    }
+	
+    public void SetDefaults()
+    {
+        volumeSlider.value = 0.8f;
+        difficultySlider.value = 2f;
+    }
+
+	public void SaveAndExit()
+    {
+        PlayerPrefsManager.SetMasterVolume(volumeSlider.value);
+        PlayerPrefsManager.SetDifficulty(difficultySlider.value);
+
+        levelManager.LoadLevel("01a Start");
+    }
+}
