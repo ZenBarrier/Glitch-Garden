c050a8760e1593d26781fce42774702d95693e5e
ZenBarrier <anthony.j.barrera@gmail.com>
2016-07-07 01:40:41 -0400

    Attackers walking

---
+++ Assets/Scripts/Attackers.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Attackers : MonoBehaviour {
+
+    [Range(-1f,3f)]
+    public float walkingSpeed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.Translate(Vector2.left * walkingSpeed * Time.deltaTime);
+	}
+}
