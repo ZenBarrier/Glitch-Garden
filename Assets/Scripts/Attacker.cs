63510356b6d455d5fa71d7ff5afad23c735d692a
ZenBarrier <anthony.j.barrera@gmail.com>
2016-07-07 17:37:04 -0400

    Colliders added

--- Assets/Scripts/Attacker.cs
+++ Assets/Scripts/Attacker.cs
@@ -1,18 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Attackers : MonoBehaviour {
+public class Attacker : MonoBehaviour {
 
-    [Range(-1f,3f)]
+    [Range(0f,3f)]
     public float walkingSpeed;
 
 	// Use this for initialization
 	void Start () {
-	
+        Rigidbody2D myRigidBody = gameObject.AddComponent<Rigidbody2D>();
+        myRigidBody.isKinematic = true;
 	}
 	
 	// Update is called once per frame
 	void Update () {
         this.transform.Translate(Vector2.left * walkingSpeed * Time.deltaTime);
 	}
+
+    void OnTriggerEnter2D()
+    {
+        Debug.Log(name);
+    }
 }
