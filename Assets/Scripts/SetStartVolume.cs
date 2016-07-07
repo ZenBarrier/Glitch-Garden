5e0038f811f43cd3de72015623ac70cb1be9fa87
ZenBarrier <anthony.j.barrera@gmail.com>
2016-07-06 22:45:23 -0400

    Start Volume Fix

---
+++ Assets/Scripts/SetStartVolume.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetStartVolume : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        MusicManager music = GameObject.FindObjectOfType<MusicManager>();
+        music.SetVolume(PlayerPrefsManager.GetMasterVolume());
+    }
+}
