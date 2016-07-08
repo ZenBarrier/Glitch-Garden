7ef4d7609d532a1d642e1cbde985848f7de475a1
ZenBarrier <anthony.j.barrera@gmail.com>
2016-07-07 23:26:57 -0400

    Attacker animation events

--- Assets/Scripts/Attacker.cs
+++ Assets/Scripts/Attacker.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class Attacker : MonoBehaviour {
 
     [Range(0f,3f)]
-    public float walkingSpeed;
+    public float currentSpeed = 0.5f;
 
 	// Use this for initialization
 	void Start () {
@@ -14,11 +14,21 @@ public class Attacker : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        this.transform.Translate(Vector2.left * walkingSpeed * Time.deltaTime);
+        this.transform.Translate(Vector2.left * currentSpeed * Time.deltaTime);
 	}
 
     void OnTriggerEnter2D()
     {
         Debug.Log(name);
+    }
+
+    public void SetSpeed(float speed)
+    {
+        currentSpeed = speed;
+    }
+
+    public void StrikeCurrentTarget(float damage)
+    {
+        Debug.Log(name +" caused damage: "+ damage);
     }
 }
