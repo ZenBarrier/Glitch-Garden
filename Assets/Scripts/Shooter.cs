ef483b4ce24131b934c21c9bde5cdae6df6fba6c
ZenBarrier <anthony.j.barrera@gmail.com>
2016-07-11 15:17:19 -0400

    Shoot Projectiles

---
+++ Assets/Scripts/Shooter.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shooter : MonoBehaviour {
+
+    public GameObject projectile, projectileParent;
+
+	private void ShootGun()
+    {
+        Transform gun = transform.FindChild("Gun");
+        GameObject bullet = Instantiate(projectile, gun.position, Quaternion.identity) as GameObject;
+        bullet.transform.parent = projectileParent.transform;
+    }
+}
