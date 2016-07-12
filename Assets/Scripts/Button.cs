a0b22f0b6d30d9972e7f0eba1b7014a606642fa7
ZenBarrier <anthony.j.barrera@gmail.com>
2016-07-11 21:53:59 -0400

    Defender Buttons

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Button : MonoBehaviour {
+
+    public static GameObject selectedDefender;
+
+    public GameObject defenderPrefab;
+    
+    private Button[] buttonArray;
+
+	// Use this for initialization
+	void Start () {
+        buttonArray = GameObject.FindObjectsOfType<Button>();
+	}
+
+    void OnMouseDown()
+    {
+        foreach (Button thisButton in buttonArray)
+        {
+            thisButton.GetComponent<SpriteRenderer>().color = Color.black;
+        }
+        this.GetComponent<SpriteRenderer>().color = Color.white;
+        selectedDefender = defenderPrefab;
+    }
+}
