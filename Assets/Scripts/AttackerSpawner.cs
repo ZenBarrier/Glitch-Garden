f8311bcbabdaefca2807d914d7badfedb6232c43
ZenBarrier <anthony.j.barrera@gmail.com>
2016-07-12 16:41:58 -0400

    Spawn Attackers without delay

---
+++ Assets/Scripts/AttackerSpawner.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AttackerSpawner : MonoBehaviour {
+    public GameObject[] attackers;
+
+    private GameObject attackerParent;
+
+	// Use this for initialization
+	void Start () {
+        attackerParent = GameObject.Find("Attackers");
+        if (!attackerParent)
+        {
+            attackerParent = new GameObject("Attackers");
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        foreach (GameObject attacker in attackers)
+        {
+            GameObject childAttacker = Instantiate(attacker) as GameObject;
+            childAttacker.transform.position = this.transform.position;
+            childAttacker.transform.parent = attackerParent.transform;
+        }
+	}
+}
