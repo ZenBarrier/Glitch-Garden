03890db978cc15995c5af2414f6c06ba0cea753d
ZenBarrier <anthony.j.barrera@gmail.com>
2016-07-12 16:48:00 -0400

    Parent attackers to spawner

--- Assets/Scripts/AttackerSpawner.cs
+++ Assets/Scripts/AttackerSpawner.cs
@@ -4,16 +4,6 @@ using System.Collections;
 public class AttackerSpawner : MonoBehaviour {
     public GameObject[] attackers;
 
-    private GameObject attackerParent;
-
-	// Use this for initialization
-	void Start () {
-        attackerParent = GameObject.Find("Attackers");
-        if (!attackerParent)
-        {
-            attackerParent = new GameObject("Attackers");
-        }
-	}
 	
 	// Update is called once per frame
 	void Update () {
@@ -21,7 +11,7 @@ public class AttackerSpawner : MonoBehaviour {
         {
             GameObject childAttacker = Instantiate(attacker) as GameObject;
             childAttacker.transform.position = this.transform.position;
-            childAttacker.transform.parent = attackerParent.transform;
+            childAttacker.transform.parent = this.transform;
         }
 	}
 }
