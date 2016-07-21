0edf28a775142af7a1dedf3ff9349adf5d4107de
ZenBarrier <anthony.j.barrera@gmail.com>
2016-07-21 19:54:38 -0400

    Lose When Attacker reaches left side

---
+++ Assets/Scripts/LoseCollider.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoseCollider : MonoBehaviour {
+
+    public LevelManager levelManager;
+
+	void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.GetComponent<Attacker>())
+        {
+            levelManager.LoadLevel("03b Lose");
+        }
+    }
+}
